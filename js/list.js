e6f9d84f06f2244007108c0da1cc28da943116f5
Esraa mahmoud <62356200+Esraamahmoud12@users.noreply.github.com>
2020-08-24 22:33:02 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,4 +1,12 @@
+const clear = document.querySelector(".clear")
 const dateElement = document.getElementById("date")
+const list = document.getElementById("list")
+const input = document.getElementById("input")
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "linethrough";
+
 const options = {weekday : "long", month:"short" , day:"numeric"};
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-us",options);
@@ -9,9 +17,13 @@ let theAddButton = document.querySelector(".add-task .plus");
 let tasksContainer = document.querySelector(".tasks-content");
 let tasksCount = document.querySelector(".tasks-count span");
 let tasksCompleted = document.querySelector(".tasks-completed span");
+
 window.onload = function () {
   theInput.focus();
 };
+
+// [1] array to store the data of tasks
+var to_dos = [];
 
 // Adding The Task
 theAddButton.onclick = function () {
@@ -23,40 +35,11 @@ theAddButton.onclick = function () {
     
   } else {
     
-    let noTasksMsg = document.querySelector(".no-tasks-message");
+    // [2] store the value of input field in the array
+    to_dos.push(theInput.value);
     
-    // Check If Span With No Tasks Message Is Exist
-    if (document.body.contains(document.querySelector(".no-tasks-message"))) {
-      noTasksMsg.remove();
-      
-    }
-    
-    // Create Main Span Element
-    let mainSpan = document.createElement("span");
-    
-    // Create Delete Button
-    let deleteElement = document.createElement("span");
-    
-    // Create The Main Span Text
-    let text = document.createTextNode(theInput.value);
-    let deleteText = document.createTextNode("Delete");
-    
-    // Add Text To Main Span
-    mainSpan.appendChild(text);
-    
-    // Add Class To Main Span
-    mainSpan.className = 'task-box';
-    
-    // Add Text To Delete Button
-    deleteElement.appendChild(deleteText);
-    
-    // Add Class To Delete Button
-    deleteElement.className = 'delete';
-    // Add Delete Button To Main Span
-    mainSpan.appendChild(deleteElement);
-    
-    // Add The Task To The Container
-    tasksContainer.appendChild(mainSpan);
+    // [3] to create all tasks by looping on the array 
+    createTodoElement();
     
     // Empty The Input
     theInput.value = '';
@@ -70,7 +53,69 @@ theAddButton.onclick = function () {
   }
   
 };
+
+function createTodoElement(){
+  
+  // check the array not empty
+  if(to_dos.length !== 0){
+    console.log(to_dos);
+    
+    // to clear the box of tasks to add the new uptade of the array's data
+    tasksContainer.innerHTML = " ";
+    
+    // start no Tasks massage
+    let noTasksMsg = document.querySelector(".no-tasks-message");
+    
+    // Check If Span With No Tasks Message Is Exist
+    if (document.body.contains(document.querySelector(".no-tasks-message"))) { 
+      noTasksMsg.remove();
+    }
+    // end no Tasks massage
+    
+    // loop by using foreach to get all data of the array and get its index
+    to_dos.forEach((to_do ,toindex) => {
+      
+      // Create Main Span Element
+      let mainSpan = document.createElement("span");
+      
+      // Create Delete Button
+      let deleteElement = document.createElement("span");
+      
+      // Create The Main Span Text and put evry data element on the task box
+      let text = document.createTextNode(to_do); 
+      
+      // creat the delet button
+      let deleteText = document.createTextNode("Delete");
+      
+      // Add Text To Main Span
+      mainSpan.appendChild(text);
+      
+      // Add Class To Main Span
+      mainSpan.className = 'task-box';
+      
+      // add index attribute to be editable on the array or delet
+      mainSpan.setAttribute('index',toindex);
+      
+      // Add Text To Delete Button
+      deleteElement.appendChild(deleteText);
+      
+      // Add Class To Delete Button
+      deleteElement.className = 'delete';
+      
+      // Add Delete Button To Main Span
+      mainSpan.appendChild(deleteElement);
+      
+      // Add The Task To The Container
+      tasksContainer.appendChild(mainSpan);
+      
+    });
+  }
+}
+
 document.addEventListener('click', function (e) {
+  
+  // [1] select task box to get the index attribute
+  let mainSpan = document.querySelector(".task-box");
   
   // Delete Task
   if (e.target.className == 'delete') {
@@ -78,7 +123,13 @@ document.addEventListener('click', function (e) {
     // Remove Current Task
     e.target.parentNode.remove();
     
-    // Check Number Of Tasks Inside The Container
+    // [2] remove current task from the data array
+    to_dos = to_dos.filter((to_do,index) => index != mainSpan.getAttribute("index") ); 
+    console.log(to_dos);
+    
+    createTodoElement();
+    
+    // Check Number Of Tasks Inside The Container 
     if (tasksContainer.childElementCount == 0) {
       
       createNoTasks();
@@ -86,6 +137,7 @@ document.addEventListener('click', function (e) {
     }
     
   }
+  
   // Finish Task
   if (e.target.classList.contains('task-box')) {
     
@@ -127,76 +179,3 @@ function calculateTasks() {
   tasksCompleted.innerHTML = document.querySelectorAll('.tasks-content .finished').length;
   
 }  
-var to_dos = [];
-
-// Adding The Task
-theAddButton.onclick = function () {
-  
-  // If Input is Empty
-  if (theInput.value === '') {
-    
-    console.log("No Value");
-    
-  } else {
-
-    to_dos.push(theInput.value);
-    
-    createTodoElement();
-
-    // Empty The Input
-    theInput.value = '';
-    
-    // Focus On Field
-    theInput.focus();
-    
-    // Calculate Tasks
-    calculateTasks();
-    
-  }
-  
-};
-
-function createTodoElement(){
-  
-  if(to_dos.length !== 0){
-    console.log(to_dos);
-    tasksContainer.innerHTML = " ";
-
-    let noTasksMsg = document.querySelector(".no-tasks-message");
-      
-      // Check If Span With No Tasks Message Is Exist
-      if (document.body.contains(document.querySelector(".no-tasks-message"))) { 
-        noTasksMsg.remove();
-      }
-  
-      to_dos.forEach(to_do => {
-          // Create Main Span Element
-        let mainSpan = document.createElement("span");
-        
-        // Create Delete Button
-        let deleteElement = document.createElement("span");
-        
-        // Create The Main Span Text
-        let text = document.createTextNode(to_do);
-        let deleteText = document.createTextNode("Delete");
-        
-        // Add Text To Main Span
-        mainSpan.appendChild(text);
-        
-        // Add Class To Main Span
-        mainSpan.className = 'task-box';
-        
-        // Add Text To Delete Button
-        deleteElement.appendChild(deleteText);
-        
-        // Add Class To Delete Button
-        deleteElement.className = 'delete';
-        // Add Delete Button To Main Span
-        mainSpan.appendChild(deleteElement);
-        
-        // Add The Task To The Container
-        tasksContainer.appendChild(mainSpan);
-        
-      });
-  }
-}
